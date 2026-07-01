213904f109671f83f79f5dcb6a72a2530a4601db
LightCoder1710 <hoanggiabaole2007@gmail.com>
2026-07-01 17:27:40 +0700

    variable

--- src/bin/printrust.rs
+++ src/bin/printrust.rs
@@ -1,5 +1,8 @@
 fn main () {
+    // this is comment
     println!("Hello con me may");
     println!("Nothing to say ");
     println!("rust is new language \n and i love this ");
+    // this is comment too;
+    /* and this one is comment too */
 }
---
+++ src/bin/variable.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let name = "light";
+    let age = 18;
+    let job = "student";
+    let school = "VKU";
+    let concu= 10;
+    let mut xnxx = 36;
+    println!("my name is {} and my age is {}, im a {} and im studing at {}", name, age, job, school);
+    println!("{}",concu);
+    println!("{}", xnxx);
+    xnxx=67;
+    println!("{}", xnxx);
+}
