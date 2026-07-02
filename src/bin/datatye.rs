b335f29f7141ac47766c92294c87b3f56e07ced0
LightCoder1710 <hoanggiabaole2007@gmail.com>
2026-07-02 23:15:38 +0700

    datatype

---
+++ src/bin/datatye.rs
@@ -0,0 +1,11 @@
+fn main () {
+    let so= 10; 
+    println!("Age is: {}", so);
+
+    let name = "John";
+    let age = 28;
+    let is_admin = false;
+    println!("Name: {}", name);
+    println!("Age: {}", age);
+    println!("Admin: {}", is_admin);
+}
