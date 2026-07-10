c9a3e63ee2937aeb0cb356715f127d426604c12e
LightCoder1710 <hoanggiabaole2007@gmail.com>
2026-07-10 20:25:45 +0700

    just loop

---
+++ src/bin/loop.rs
@@ -0,0 +1,19 @@
+fn main () {
+    let mut chim = 1;
+    while chim <= 4 {
+        println!("chim is {}", chim);
+        chim += 2;
+    }
+    loop {
+        if chim <= 20 {
+            println!( "sasuke naruto");
+             chim +=4;
+        }
+        else {
+            
+            break;
+        }
+        
+       
+    }
+}
